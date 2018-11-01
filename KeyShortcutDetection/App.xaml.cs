7ce6499488a976c9b16f00d11727bdec8a76a3cc
John VanSickle <jwvansickle@icloud.com>
2018-11-01 18:49:31 -0400

    initial commit

---
+++ KeyShortcutDetection/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace KeyShortcutDetection
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            Element.SetMenu(this, new Menu());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
